7d688a31fa72129feabd71dc9e5ce1afd5699a23
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-24 21:25:05 +0300

    created review page component

--- client/src/components/ReviewCard.tsx
+++ client/src/components/ReviewCard.tsx
@@ -1,10 +1,11 @@
+import ReviewStar from "@app/assets/icons/ReviewStar";
 import { useTranslation } from "react-i18next";
 
 const ReviewCard = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="delay-30 box-shadow-cardShadow w-[320px] transform cursor-pointer overflow-hidden rounded shadow-lg transition ease-in hover:scale-105 dark:bg-[#2C2C2C]">
+    <div className="delay-30 w-[320px] transform cursor-pointer overflow-hidden rounded-[8px] shadow-cardShadow transition ease-in hover:scale-105 dark:bg-[#2C2C2C]">
       <img
         className="h-[194px] w-full"
         src="/testimage.jpg"
@@ -15,53 +16,7 @@ const ReviewCard = () => {
           <div className="flex text-base font-bold dark:text-white">
             Polaroid Land Camer...
           </div>
-          <div className="flex items-center space-x-1">
-            <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="h-4 w-4 text-gray-300 dark:text-gray-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-          </div>
+          <ReviewStar />
         </div>
         <div className="flex justify-start font-medium dark:text-white">
           Movies
@@ -73,8 +28,16 @@ const ReviewCard = () => {
           <span className="ml-1 dark:text-white">Photo</span>
         </div>
         <div className="flex justify-start text-sm">
-          <span className="font-medium dark:text-white">Tags:</span>
+          <span className="font-medium dark:text-white">
+            {`${t("Review.tags")}:`}
+          </span>
           <span className="ml-1 dark:text-white">#photo #art #fashion</span>
+        </div>
+        <div className="flex justify-start text-sm">
+          <span className="font-medium dark:text-white">
+            {`${t("Review.authorGrade")}:`}
+          </span>
+          <span className="ml-1 dark:text-white">6</span>
         </div>
         <div className="flex justify-between">
           <div className="flex justify-start text-sm">
