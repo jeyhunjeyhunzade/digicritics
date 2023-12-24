36a8c53f7dd549e04a51e6fbfce72ffe084f0cb6
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-12-24 20:55:12 +0300

    relocated backend service to Render

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -1,4 +1,4 @@
 export const serverUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000"
-    : "https://digicritics-server-76f60f3f49d2.herokuapp.com";
+    : "https://digicritics-server.onrender.com";
